704db6365ea5fc04e451e1532d790e86ed118f62
Sakonwatzzz <650112418026@gmail.com>
2023-12-07 15:52:50 +0700

    LAB3/2

--- LAB3/html/editform.php
+++ LAB3/html/editform.php
@@ -1,26 +1,28 @@
 <?php
-include("condb.php");
+include "condb.php";
 $id = $_GET['id'];
 
-$sql = "SELECT * FROM tb_member WHERE id_member='$id'";
+ $sql = "SELECT * FROM tb_member WHERE id_member='$id'";
 $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_assoc($result);
 ?>
-<form id="editfrom">
+
+<form id="Editform">
     <label for="inp_id">ID</label>
     <input type="text" name="id" id="inp_id" readonly value="<?= $row['id_member'] ?>"> <br>
     <label for="inp_name">NAME</label>
-    <input type="text" name="name" id="inp_name" value="<?= $row['name'] ?>"><br>
+    <input type="text" name="name" id="inp_name" value="<?= $row['name'] ?>"> <br>
     <label for="inp_prov">PROVINCE</label>
-    <input type="text" name="prov" id="inp_prov" value="<?= $row['id_province'] ?>"><br>
+    <input type="text" name="prov" id="inp_prov" value="<?= $row['id_province'] ?>"> <br>
     <hr>
-    <button type="submit" class="btn btn-primary"> SAVE </button>
-    <button type="reset"class="btn btn-dager"> CANCLE </button>
+    <button type="submit" class="btn btn-primary">SAVE</button>
+    <button type="reset" class="btn btn-danger">CANCLE</button>
 </form>
 
 <script>
     $("form").submit(function(e) {
         e.preventDefault();
+
         let fm = $(this);
         $.ajax({
             url: "/edititem.php",
@@ -28,15 +30,14 @@ $row = mysqli_fetch_assoc($result);
             data: fm.serialize(),
             success: function(res) {
                 console.log(res);
+
                 if (res == "error")
                     alert("Don't insert data into DB.");
                 else {
                     $("#div_item").load("/listitem.php");
                     $('#staticBackdrop').modal('hide')
-
                 }
             }
-
         });
     });
 </script>
