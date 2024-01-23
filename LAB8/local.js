e168d72f4e2d65cf287e9531ffbe5a9a394196b3
Sakonwatzzz <650112418026@gmail.com>
2024-01-23 15:31:40 +0700

    LAB8

---
+++ LAB8/local.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+http
+    .createServer(function (req,res){
+        res.writeHead(200, {"Content-type": "text/plain"});
+        res.write("Hello World!");
+        res.end();
+    })
+    .listen(3000)
